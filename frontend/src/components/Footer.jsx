85ebfdcc34469129652307e5110b269abb6071eb
eioforkansi <eioforkansi@gmail.com>
2025-01-15 18:37:10 +0100

    Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,15 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="logo">
+          <Link to="/">
+            <h1>HealthBlog</h1>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
